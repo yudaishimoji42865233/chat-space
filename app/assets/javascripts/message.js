b93fdf119af2934f45eef37646f1efafab047e5f
yudaishimoji42865233 <yudai.shimoji.42865233@gmail.com>
2019-10-13 13:48:07 +0900

    完成

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -39,7 +39,7 @@ $(function(){
      return html;
    };
  }
-$('.js-form').on('submit', function(e){
+$('#new_message').on('submit', function(e){
  e.preventDefault();
  var formData = new FormData(this);
  var url = $(this).attr('action')
@@ -58,7 +58,7 @@ $('.js-form').on('submit', function(e){
     $('form')[0].reset();
   })
    .fail(function(){
-     alert('error');
+     alert('自動更新に失敗しました');
    });
    return false;
  });
