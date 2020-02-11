adc8f14c1108c2afa39900661a0a7ef0952b6b22
yudaishimoji42865233 <yudai.shimoji.42865233@gmail.com>
2020-02-12 02:18:55 +0900

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '3.115.57.35', user: 'ec2-user', roles: %w{app db web}
+server '18.178.42.193', user: 'ec2-user', roles: %w{app db web}
