06ea0dfe0b0f98a738412d38267a98a0530f1b9b
yudaishimoji42865233 <yudai.shimoji.42865233@gmail.com>
2020-02-12 12:02:35 +0900

    update deploy.rb

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,8 +5,11 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   process resize_to_fit: [800, 800]
   # Choose what kind of storage to use for this uploader:
-  storage :fog
-  # storage :fog
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -36,14 +39,21 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
+
+  version :thumb do
+    process resize_to_fit: [80, 80]
+  end
+
+  version :content do
+    process resize_to_limit: [640, 640]
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   # def filename
   #   "something.jpg" if original_filename
   # end
-  process resize_to_fit: [800, 800]
 end
--- config/deploy.rb
+++ config/deploy.rb
@@ -80,12 +80,12 @@ end
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
 
-set :default_env, {
-  rbenv_root: "/usr/local/rbenv",
-  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
-  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
-  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
-}
+# set :default_env, {
+  # rbenv_root: "/usr/local/rbenv",
+  # path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  # AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  # AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
+# }
 
 
 
